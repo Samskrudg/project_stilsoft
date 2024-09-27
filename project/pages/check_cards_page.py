3d667cabcf05573ae25d4f48af825fcf81293df8
снег <sam.skrudgi@gmail.com>
2024-09-27 16:22:00 +0700

    add cycle transition card product

--- project/pages/check_cards_page.py
+++ project/pages/check_cards_page.py
@@ -1,11 +1,18 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import
 
 
 class CheckCardsPage(BasePage):
     def go_to_product_page(self):
-        product_link = self.browser.find_element(*MainPageLocators.CARD_PRODUCT)
+        product_link = self.browser.find_elements(*MainPageLocators.CARD_PRODUCT)
         product_link.click()
+
+    def back_browser(self):
+        return_catalog = self.browser
+        return_catalog.back()
 
     def take_current_url(self):
         self.current_url = self.browser.current_url
@@ -24,10 +31,18 @@ class CheckCardsPage(BasePage):
 
     def check_requirements(self):
         self.take_current_url()
-        try:
-            self.check_image_presence()
-            self.check_assigned_service_life()
-        except AssertionError as e:
-            print(f"Проверка не пройдена: {e}")
-            self.write_product_file()
+        count = 0
+        web_elements = self.go_to_product_page()[count]
+        for element in range(len(web_elements)):
+             web_elements[count].click()
+             self.back_browser()
+             web_elements = WebDriverWait(browser, 10).until(
+                 EC.presence_of_all_elements_located((*MainPageLocators.CARD_PRODUCT)))
+             count += 1
+            try:
+                self.check_image_presence()
+                self.check_assigned_service_life()
+            except AssertionError as e:
+                print(f"Проверка не пройдена: {e}")
+                self.write_product_file()
 
--- project/pages/locators.py
+++ project/pages/locators.py
@@ -2,6 +2,6 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators:
-    CARD_PRODUCT = (By.ID, "product_5448")
+    CARD_PRODUCT = (By.CLASS_NAME, "product")
     IMAGE_PRODUCT = (By.CLASS_NAME, "imgCont")
     SERVICE_LIFE = (By.XPATH, '//*[@id="tab-1"]/ul[4]/li[2]')
